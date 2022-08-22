1871b99b134ef07e37e6b754f618ea1999ccf478
jmoda1028 <jmoda1028@gmail.com>
2022-08-23 07:45:26 +0800

    added uploading image in product

--- api/models.py
+++ api/models.py
@@ -96,6 +96,7 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     qty_on_hand = models.IntegerField()
     price= models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='product_images', blank=True, null=True)
     date_stock_in = models.DateField(auto_now=False, auto_now_add=False)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
