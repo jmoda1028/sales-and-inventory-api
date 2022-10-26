0288724d7d2c2e41f374f135ea8077f12d80125e
jmoda1028 <jmoda1028@gmail.com>
2022-10-27 04:09:52 +0800

    gumana kana sana

--- core_app/settings.py
+++ core_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','sales-and-inventory-app.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','sales-and-inventory-api.herokuapp.com']
 
 # Application definition
 
