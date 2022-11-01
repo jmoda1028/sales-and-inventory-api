1936b543779d9b610f997b84d99777da864cb9fb
jmoda1028 <jmoda1028@gmail.com>
2022-11-01 17:19:52 +0800

    update

--- core_app/settings.py
+++ core_app/settings.py
@@ -213,15 +213,17 @@ CORS_ALLOW_CREDENTIALS = True
 #     'x-requested-with',
 # )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# SMTP Mail service with decouple
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_HOST_USER = 'jun.moda93@gmail.com'
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jun.moda93@gmail.com'
-EMAIL_HOST_PASSWORD = 'Haffsah@1028!'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-# EMAIL_HOST = '0.0.0.0'
-# EMAIL_PORT = 1025
+EMAIL_HOST = '0.0.0.0'
+EMAIL_PORT = 1025
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
